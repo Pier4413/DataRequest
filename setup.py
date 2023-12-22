d39ed48cbdbbcbec7b71d76f4628f36788b4bcad
Pier4413 <pierre.delmas@delmasweb.net>
2023-12-22 15:04:23 +0100

    Add the module creation and upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='data_request',
+    packages=['data_request'],
+    description='A simple wrapper to make HTTP Requests',
+    version='1.0.7',  # updated
+    url='',
+    author='Panda',
+    author_email='panda@delmasweb.net',
+    keywords=['http', 'request', 'api']
+)
