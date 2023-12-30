8db6e66e7bde773b3a3ed56da86d97e8d05e6f66
Pier4413 <pierre.delmas@delmasweb.net>
2023-12-30 18:07:22 +0100

    Update setup and delete a useless file

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
     name='data_request',
     packages=['data_request'],
     description='A simple wrapper to make HTTP Requests',
-    version='1.0.7',  # updated
-    url='',
+    version='1.0.8',  # updated
+    url='https://github.com/Pier4413/DataRequest',
     author='Panda',
     author_email='panda@delmasweb.net',
+    install_requires=[
+        'requests'
+    ],
     keywords=['http', 'request', 'api']
 )
