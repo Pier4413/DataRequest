87d362ddb80c60eb684aff058b15e15e54169cc6
Pier4413 <pierre.delmas@delmasweb.net>
2025-05-20 17:14:03 +0000

    Add the version in the metadata package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
-
+from data_request.metadata.__version__ import __version__
 setup(
     name='data_request',
     packages=['data_request'],
     description='A simple wrapper to make HTTP Requests',
-    version='1.0.8',  # updated
+    version=__version__,  # updated
     url='https://github.com/Pier4413/DataRequest',
     author='Panda',
     author_email='panda@delmasweb.net',
